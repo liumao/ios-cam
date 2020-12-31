afc8888628c9aa829ff64b71be28eb280006cdd0
liumao <loveniu13140@gmail.com>
2020-12-31 10:37:14 +0800

    add Application supports iTunes file sharing = YES

--- Cam/ViewController.swift
+++ Cam/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 import AssetsLibrary
 import MobileCoreServices
 
@@ -47,45 +48,60 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //private media
         let infodic:NSDictionary = info as NSDictionary
         let mediaUrl: NSURL = infodic[UIImagePickerController.InfoKey.mediaURL] as! NSURL
-        let videoPath = mediaUrl.path!
-        NSLog("video path: %@", videoPath)
         
-        //save
-        if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(videoPath) {
-            UISaveVideoAtPathToSavedPhotosAlbum(videoPath, nil, nil, nil)
+        //create album
+        let fetchOptions = PHFetchOptions()
+        let pidAlbm = "PID" + cameraView.pidView.text!
+        fetchOptions.predicate = NSPredicate(format: "title = %@", pidAlbm);
+        let collection : PHFetchResult = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+        var assetCollection: PHAssetCollection!
+        var assetCollectionPlaceholder: PHObjectPlaceholder!
+        if let _: AnyObject = collection.firstObject {
+            assetCollection = collection.firstObject!
+        } else {
+            PHPhotoLibrary.shared().performChanges({
+                let createAlbumRequest : PHAssetCollectionChangeRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: pidAlbm);
+                assetCollectionPlaceholder = createAlbumRequest.placeholderForCreatedAssetCollection
+                }, completionHandler: { (isSuccess: Bool, error: Error?) in
+                    if (isSuccess) {
+                        let collectionFetchResult = PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [assetCollectionPlaceholder.localIdentifier], options: nil)
+                        assetCollection = collectionFetchResult.firstObject!
+                    } else {
+                        
+                    }
+                }
+            )
         }
         
         //rename
+        let videoPath = mediaUrl.path!
         var newVideoPath = String(videoPath.prefix(upTo: videoPath.lastIndex(of: "/")!))
         //get file name
         let fileName = cameraView.getFileName()
-        
-        //file manager
-        let fileManager = FileManager()
-        
-        //create dir
-        let pid = cameraView.pidView.text!
-        let pidDir = newVideoPath + "/PID" + pid
-        if !fileManager.fileExists(atPath: pidDir) {
-            do {
-                try fileManager.createDirectory(atPath: pidDir, withIntermediateDirectories: true, attributes:nil)
-                newVideoPath = pidDir + "/" + fileName
-            } catch {
-                newVideoPath = newVideoPath + "/" + fileName
-                NSLog("rename failure")
-            }
-        } else {
-            newVideoPath = pidDir + "/" + fileName
-        }
+        newVideoPath = newVideoPath + "/" + fileName
         NSLog("new video path: %@", newVideoPath)
         
-        do {
-          try fileManager.moveItem(atPath: videoPath, toPath: newVideoPath)
-        } catch {
-            NSLog("rename failure")
+        //save
+        if assetCollection != nil {
+            PHPhotoLibrary.shared().performChanges({
+                let result = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: mediaUrl as URL)
+                let assetPlaceholder = result?.placeholderForCreatedAsset
+                let albumChangeRequset = PHAssetCollectionChangeRequest(for: assetCollection!)
+                albumChangeRequset!.addAssets([assetPlaceholder!] as NSArray)
+            }, completionHandler: { (isSuccess: Bool, error: Error?) in
+                if isSuccess {
+                    NSLog("save success.")
+                } else {
+                    NSLog("save failure.  %@", error!.localizedDescription)
+                }
+            })
+        } else {
+            NSLog("album %@ is null", pidAlbm)
         }
+        
         picker.dismiss(animated: true, completion: nil)
     }
     
