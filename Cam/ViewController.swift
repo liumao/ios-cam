e72f7d088cd187f69a0d7b85d4944cc0252c566d
makaloLau <loveniu13140@gmail.com>
2021-01-04 22:44:44 +0800

    1、保存到沙盒到同时保存视频到相册，以供测试人员定位偏移量。

--- Cam/ViewController.swift
+++ Cam/ViewController.swift
@@ -7,6 +7,7 @@
 //  Copyright © 2020年 MakaloLau. All rights reserved.
 //
 import UIKit
+import Photos
 import AssetsLibrary
 import MobileCoreServices
 
@@ -41,30 +42,28 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @objc func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-            //收起键盘
             cameraView.pidView.resignFirstResponder()
             return true;
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //media url
         let infodic:NSDictionary = info as NSDictionary
         let mediaUrl: NSURL = infodic[UIImagePickerController.InfoKey.mediaURL] as! NSURL
         let videoPath = mediaUrl.path!
         NSLog("video path: %@", videoPath)
-        
+
         //save
-        if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(videoPath) {
-            UISaveVideoAtPathToSavedPhotosAlbum(videoPath, nil, nil, nil)
-        }
+        createAlbumAndSaveVideo(mediaUrl: mediaUrl)
         
         //rename
         var newVideoPath = String(videoPath.prefix(upTo: videoPath.lastIndex(of: "/")!))
         //get file name
         let fileName = cameraView.getFileName()
-        
+
         //file manager
         let fileManager = FileManager()
-        
+
         //create dir
         let pid = cameraView.pidView.text!
         let pidDir = newVideoPath + "/PID" + pid
@@ -80,13 +79,62 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             newVideoPath = pidDir + "/" + fileName
         }
         NSLog("new video path: %@", newVideoPath)
-        
+
         do {
-          try fileManager.moveItem(atPath: videoPath, toPath: newVideoPath)
+          try fileManager.copyItem(atPath: videoPath, toPath: newVideoPath)
         } catch {
             NSLog("rename failure")
         }
+        
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func createAlbumAndSaveVideo(mediaUrl: NSURL) {
+        //create album
+        let fetchOptions = PHFetchOptions()
+        let pidAlbm = "PID" + cameraView.pidView.text!
+        fetchOptions.predicate = NSPredicate(format: "title = %@", pidAlbm);
+        let collection : PHFetchResult = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+        var assetCollection: PHAssetCollection!
+        var assetCollectionPlaceholder: PHObjectPlaceholder!
+        if let _: AnyObject = collection.firstObject {
+            assetCollection = collection.firstObject!
+            
+            //save to album
+            saveVideoToAlbum(assetCollection: assetCollection, mediaUrl: mediaUrl)
+        } else {
+            PHPhotoLibrary.shared().performChanges({
+                let createAlbumRequest : PHAssetCollectionChangeRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: pidAlbm);
+                assetCollectionPlaceholder = createAlbumRequest.placeholderForCreatedAssetCollection
+                }, completionHandler: { (isSuccess: Bool, error: Error?) in
+                    if (isSuccess) {
+                        let collectionFetchResult = PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [assetCollectionPlaceholder.localIdentifier], options: nil)
+                        assetCollection = collectionFetchResult.firstObject!
+                        NSLog("create success.")
+                        
+                        //save to album
+                        self.saveVideoToAlbum(assetCollection: assetCollection, mediaUrl: mediaUrl)
+                    } else {
+                        NSLog("create failure.")
+                    }
+                }
+            )
+        }
+    }
+    
+    func saveVideoToAlbum(assetCollection: PHAssetCollection, mediaUrl: NSURL) {
+        PHPhotoLibrary.shared().performChanges({
+            let result = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: mediaUrl as URL)
+            let assetPlaceholder = result?.placeholderForCreatedAsset
+            let albumChangeRequset = PHAssetCollectionChangeRequest(for: assetCollection)
+            albumChangeRequset!.addAssets([assetPlaceholder!] as NSArray)
+        }, completionHandler: { (isSuccess: Bool, error: Error?) in
+            if isSuccess {
+                NSLog("save success.")
+            } else {
+                NSLog("save failure.  %@", error!.localizedDescription)
+            }
+        })
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
